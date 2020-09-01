3a49172f117ce50acadacdb2a5133e6f8dc659f1
monday-hazard <heudeclaire@ymail.com>
2020-09-01 20:42:02 +0300

    starters

---
+++ el-tales-strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f370164638dee2d079c2cb9ef9d4a6ce'),
+    },
+  },
+});
---
+++ el-tales-strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4764ad4c-4c44-413f-9a6f-4dfc51a7eb3a'
+};
